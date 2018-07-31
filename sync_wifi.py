8fe044f587f6bfba68cefe439b996fc4af280f9f
William Wold <wm@wmww.sh>
2018-07-30 23:55:27 -0700

    optimized by reducing number of nmcli subprocess calls

--- sync_wifi.py
+++ sync_wifi.py
@@ -1,3 +1,4 @@
+import sys
 import subprocess
 import re
 
@@ -34,11 +35,10 @@ def nmcli_get_network_list():
     networks = [i.strip() for i in out.stdout.strip().split('\n')[1:]] # cut off "NAME" header
     return networks
 
-def parse_from_nmcli(name):
-    out = Run([nmcli_path, '--show-secrets', 'connection', 'show', name], raise_on_fail=True)
-    ssids = re.findall('\n.*\.ssid:\s*(.*)\n', out.stdout)
-    psks = re.findall('\n.*\.psk:\s*(.*)\n', out.stdout)
-    autoconnect_nos = re.findall('\n.*\.autoconnect:\s*(no)\n', out.stdout)
+def nmcli_parse_single_network(name, data):
+    ssids = re.findall('\n.*\.ssid:\s*(.*)\n', data)
+    psks = re.findall('\n.*\.psk:\s*(.*)\n', data)
+    autoconnect_nos = re.findall('\n.*\.autoconnect:\s*(no)\n', data)
     assert len(ssids) == 1, 'could not properly detect SSID'
     assert len(psks) <= 1, 'found more then one password'
     autoconnect = len(autoconnect_nos) == 0
@@ -47,15 +47,33 @@ def parse_from_nmcli(name):
         psk = psks[0]
     return Network(name, ssids[0], psk, autoconnect)
 
-def get_all_from_nmcli():
-    names = nmcli_get_network_list()
+def nmcli_get_and_parse(names):
+    if not isinstance(names, list):
+        names = [names]
+    out = Run([nmcli_path, '--show-secrets', 'connection', 'show'] + names, raise_on_fail=True)
+    data = out.stdout.split('\n\n')
+    assert len(data) == len(names), 'nmcli gave the wrong number of networks'
     networks = []
-    for name in names:
+    for i in range(len(data)):
         try:
-            networks.append(parse_from_nmcli(name))
+            networks.append(nmcli_parse_single_network(names[i], data[i]))
         except Exception as e:
-            print('error with \'' + name + '\': ' + str(e))
+            print('error with \'' + names[i] + '\':' + str(e), file=sys.stderr)
     return networks
 
+def nmcli_get_networks():
+    names = nmcli_get_network_list()
+    try:
+        return nmcli_get_and_parse(names)
+    except Exception as e:
+        print('error:' + str(e) + ', falling back to parsing networks individually', file=sys.stderr)
+        networks = []
+        for name in names:
+            try:
+                networks.append(nmcli_get_and_parse(name))
+            except Exception as e:
+                print('error with \'' + name + '\':' + str(e), file=sys.stderr)
+        return networks
+
 if __name__ == '__main__':
-    print('\n'.join([str(i) for i in get_all_from_nmcli()]))
+    print('\n'.join([str(i) for i in nmcli_get_networks()]))
